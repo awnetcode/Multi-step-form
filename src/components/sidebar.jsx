924adfca09b31d851240232213aed937f617e9d5
awnetcode <155958835+awnetcode@users.noreply.github.com>
2024-09-11 20:29:13 +0200

    initial

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,19 @@
+import '../styles/sidebar.css';
+
+function Sidebar(){
+    return(
+        <>
+        <nav className="sidebar-navigation">
+            <ul className="sidebar-list">
+                <li className="sidebar-link">1</li>
+                <li className="sidebar-link">2</li>
+                <li className="sidebar-link">3</li>
+                <li className="sidebar-link">4</li>
+            </ul>
+        </nav>
+        
+        </>
+    )
+}
+
+export default Sidebar;
